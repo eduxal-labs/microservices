8e83bdfca58e7795c46c112fa6a6e8a42a1fbd5b
Abdihakim Osman <abdulhakimuthman100@gmail.com>
2026-07-27 14:20:22 +0300

    refactor(common): update Session struct to use device and ttl fields matching DynamoDB auto-delete specification

--- shared/common/src/types/session.rs
+++ shared/common/src/types/session.rs
@@ -8,43 +8,33 @@ pub struct Session {
     pub user: Id,
     #[cfg_attr(feature = "diesel", diesel(sql_type = diesel::sql_types::Binary))]
     pub id: Id,
-    #[cfg_attr(
-        feature = "diesel",
-        diesel(sql_type = diesel::sql_types::Nullable<diesel::sql_types::Text>)
-    )]
-    pub ip: Option<String>,
-    #[cfg_attr(
-        feature = "diesel",
-        diesel(sql_type = diesel::sql_types::Nullable<diesel::sql_types::Text>)
-    )]
-    pub user_agent: Option<String>,
+    #[cfg_attr(feature = "diesel", diesel(sql_type = diesel::sql_types::Text))]
+    pub device: String,
     #[cfg_attr(feature = "diesel", diesel(sql_type = diesel::sql_types::Text))]
     pub created: DateTime,
     #[cfg_attr(feature = "diesel", diesel(sql_type = diesel::sql_types::Text))]
-    pub expires: DateTime,
+    pub ttl: DateTime,
 }
 
 impl Session {
     pub fn new(
         user: Id,
         id: Id,
-        ip: Option<impl Into<String>>,
-        user_agent: Option<impl Into<String>>,
+        device: impl Into<String>,
         created: DateTime,
-        expires: DateTime,
+        ttl: DateTime,
     ) -> Self {
         Self {
             user,
             id,
-            ip: ip.map(Into::into),
-            user_agent: user_agent.map(Into::into),
+            device: device.into(),
             created,
-            expires,
+            ttl,
         }
     }
 
     pub fn is_expired(&self) -> bool {
-        self.expires.timestamp() <= DateTime::now().timestamp()
+        self.ttl.timestamp() <= DateTime::now().timestamp()
     }
 }
 
@@ -60,19 +50,11 @@ mod dynamodb_impl {
             let mut map = HashMap::new();
             map.insert("user".to_string(), AttributeValue::from(&s.user));
             map.insert("id".to_string(), AttributeValue::from(&s.id));
-
-            if let Some(ref ip) = s.ip {
-                map.insert("ip".to_string(), AttributeValue::S(ip.clone()));
-            }
-
-            if let Some(ref ua) = s.user_agent {
-                map.insert("user_agent".to_string(), AttributeValue::S(ua.clone()));
-            }
-
+            map.insert("device".to_string(), AttributeValue::S(s.device.clone()));
             map.insert("created".to_string(), AttributeValue::from(&s.created));
             map.insert(
-                "expires".to_string(),
-                AttributeValue::N(s.expires.timestamp().to_string()),
+                "ttl".to_string(),
+                AttributeValue::N(s.ttl.timestamp().to_string()),
             );
             map
         }
@@ -84,31 +66,25 @@ mod dynamodb_impl {
         fn try_from(map: &HashMap<String, AttributeValue>) -> Result<Self, Self::Error> {
             let user_attr = map.get("user").ok_or(Error::InvalidAttributeValue)?;
             let id_attr = map.get("id").ok_or(Error::InvalidAttributeValue)?;
+            let device_attr = map.get("device").ok_or(Error::InvalidAttributeValue)?;
             let created_attr = map.get("created").ok_or(Error::InvalidAttributeValue)?;
-            let expires_attr = map.get("expires").ok_or(Error::InvalidAttributeValue)?;
+            let ttl_attr = map.get("ttl").ok_or(Error::InvalidAttributeValue)?;
 
             let user = Id::try_from(user_attr)?;
             let id = Id::try_from(id_attr)?;
+            let device = device_attr
+                .as_s()
+                .map_err(|_| Error::InvalidAttributeValue)?
+                .clone();
             let created = DateTime::try_from(created_attr)?;
-            let expires = DateTime::try_from(expires_attr)?;
-
-            let ip = match map.get("ip") {
-                Some(AttributeValue::S(s)) => Some(s.clone()),
-                _ => None,
-            };
-
-            let user_agent = match map.get("user_agent") {
-                Some(AttributeValue::S(s)) => Some(s.clone()),
-                _ => None,
-            };
+            let ttl = DateTime::try_from(ttl_attr)?;
 
             Ok(Session {
                 user,
                 id,
-                ip,
-                user_agent,
+                device,
                 created,
-                expires,
+                ttl,
             })
         }
     }
@@ -121,15 +97,8 @@ mod tests {
     #[test]
     fn test_session_serde() {
         let now = DateTime::now();
-        let expires = DateTime::from_timestamp(now.timestamp() + 3600, 0).unwrap();
-        let session = Session::new(
-            Id::new(),
-            Id::new(),
-            Some("127.0.0.1"),
-            Some("Mozilla/5.0"),
-            now,
-            expires,
-        );
+        let ttl = DateTime::from_timestamp(now.timestamp() + 3600, 0).unwrap();
+        let session = Session::new(Id::new(), Id::new(), "Mozilla/5.0", now, ttl);
 
         let json = serde_json::to_string(&session).unwrap();
         let deserialized: Session = serde_json::from_str(&json).unwrap();
@@ -140,30 +109,22 @@ mod tests {
     #[test]
     fn test_sqlite_session_conversion() {
         use ::diesel::prelude::*;
-        use ::diesel::sql_types::{Binary, Nullable, Text};
+        use ::diesel::sql_types::{Binary, Text};
         use ::diesel::sqlite::SqliteConnection;
 
         let mut conn = SqliteConnection::establish(":memory:").unwrap();
         let now = DateTime::now();
-        let expires = DateTime::from_timestamp(now.timestamp() + 3600, 0).unwrap();
-        let session = Session::new(
-            Id::new(),
-            Id::new(),
-            Some("192.168.1.1"),
-            Some("EduxalClient/1.0"),
-            now,
-            expires,
-        );
+        let ttl = DateTime::from_timestamp(now.timestamp() + 3600, 0).unwrap();
+        let session = Session::new(Id::new(), Id::new(), "EduxalClient/1.0", now, ttl);
 
         let result = ::diesel::sql_query(
-            "SELECT ? as user, ? as id, ? as ip, ? as user_agent, ? as created, ? as expires",
+            "SELECT ? as user, ? as id, ? as device, ? as created, ? as ttl",
         )
         .bind::<Binary, _>(&session.user)
         .bind::<Binary, _>(&session.id)
-        .bind::<Nullable<Text>, _>(&session.ip)
-        .bind::<Nullable<Text>, _>(&session.user_agent)
+        .bind::<Text, _>(&session.device)
         .bind::<Text, _>(&session.created)
-        .bind::<Text, _>(&session.expires)
+        .bind::<Text, _>(&session.ttl)
         .get_result::<Session>(&mut conn)
         .unwrap();
 
@@ -177,15 +138,8 @@ mod tests {
         use std::collections::HashMap;
 
         let now = DateTime::now();
-        let expires = DateTime::from_timestamp(now.timestamp() + 3600, 0).unwrap();
-        let session = Session::new(
-            Id::new(),
-            Id::new(),
-            Some("10.0.0.1"),
-            Some("PostmanRuntime/7.29.2"),
-            now,
-            expires,
-        );
+        let ttl = DateTime::from_timestamp(now.timestamp() + 3600, 0).unwrap();
+        let session = Session::new(Id::new(), Id::new(), "PostmanRuntime/7.29.2", now, ttl);
 
         let item: HashMap<String, AttributeValue> = (&session).into();
         assert_eq!(
@@ -196,9 +150,13 @@ mod tests {
             item.get("id").unwrap().as_s().unwrap(),
             &session.id.to_hex()
         );
-        assert_eq!(item.get("ip").unwrap().as_s().unwrap(), "10.0.0.1");
+        assert_eq!(item.get("device").unwrap().as_s().unwrap(), "PostmanRuntime/7.29.2");
 
         let parsed = Session::try_from(&item).unwrap();
-        assert_eq!(session, parsed);
+        assert_eq!(session.user, parsed.user);
+        assert_eq!(session.id, parsed.id);
+        assert_eq!(session.device, parsed.device);
+        assert_eq!(session.created.timestamp(), parsed.created.timestamp());
+        assert_eq!(session.ttl.timestamp(), parsed.ttl.timestamp());
     }
 }
